ec233faba6ccbd8f33b6b7bc18e604b8586b5875
K Jithendhar reddy <142426481+Jithendhar34@users.noreply.github.com>
2025-05-08 16:34:31 +0530

    Update App.jsx

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Products from './pages/Products.jsx';
+import Products from './pages/products.jsx';
 import Cart from './pages/Cart';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
